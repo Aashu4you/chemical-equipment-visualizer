d570587034d10cdfefd2e342959bb517bc9070ff
Aashu4you <ats4219511@gmail.com>
2026-01-27 17:18:47 +0530

    Day 2 : Equipment model finalized with migrations

--- backend/equipment/models.py
+++ backend/equipment/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Equipment(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    material = models.CharField(max_length=100)
-    max_temperature = models.FloatField()
-    pressure_rating = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    equipment_name = models.CharField(max_length=100)
+    equipment_type = models.CharField(max_length=50)
+    flowrate = models.FloatField()
+    pressure = models.FloatField()
+    temperature = models.FloatField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.equipment_name
